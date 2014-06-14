6eb498bb9e5b80154d2d7fd1ac75f503947e38e4
Ros Stepanyak <ros.aphex@gmail.com>
2014-06-14 13:09:29 +0300

    cleaning repo

--- src/com/ros/workandhome/activities/views/DayOfWeekChange.java
+++
@@ -1,7 +0,0 @@
-package com.ros.workandhome.activities.views;
-
-import com.ros.workandhome.core.entities.WHDay.WHDay;
-
-public interface DayOfWeekChange {
-   public void dayOfWeekChanged(WHDay day);
-}
--- src/com/ros/workandhome/db/dao/BaseDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ros.workandhome.db.dao;
-
-public interface BaseDAO<T> {
-	    T getByID(Long ID);
-	    T save(T type);
-	    T update(T type);
-	    void delete(T type);
-	    Iterable <T> getAll();
-}
